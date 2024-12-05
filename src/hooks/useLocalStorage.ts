bfcce04a801a2f33a1694de2aed31eb066476312
Michael Gyimadu <michaelgyimadu@gmail.com>
2024-12-04 22:07:37 -0500

    removed unused variable unusedEffect

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -3,8 +3,6 @@ import { useState, useEffect } from 'react';
 export function useLocalStorage<T>(key: string, initialValue: T) {
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
-      const unusedEffect = useEffect;
-
       const item = window.localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
