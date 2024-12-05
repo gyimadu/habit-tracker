07be3dc8b572782e64b598c92b3456b062865e21
Michael Gyimadu <michaelgyimadu@gmail.com>
2024-12-05 15:56:43 -0500

    adjusted padding on navigation bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavBar: React.FC = () => {
   return (
-    <nav className="bg-gray-800 mx-auto mb-10 text-white px-4 py-2 shadow-md rounded-lg">
+    <nav className="bg-gray-800 mx-auto mb-10 text-white px-1 py-2 shadow-md rounded-lg">
       <div className="container mx-10 flex items-center">
         <h1 className="text-lg font-bold">habits.io</h1>
       </div>
