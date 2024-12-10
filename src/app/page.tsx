6b877584bccfc00c987ce52477d2cc937a6366c7
Michael Gyimadu <michaelgyimadu@gmail.com>
2024-12-10 11:29:33 -0500

    still on homepage headline rendering issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
 
       <main className="container mx-auto p-5 max-w-7xl">
           
-        <div className="flex flex-col md:flex-row items-center justify-center h-screen mt-50 pt-70 md:pt-0 md:mt-0 mb-10 text-center">
+        <div className="flex flex-col md:flex-row items-center justify-center h-screen mt-10 pt-10 md:pt-0 md:mt-0 mb-10 text-center">
           <div className="text-left w-full md:w-6/12 mb-10">
             <h1 className="text-4xl md:text-5xl font-bold mb-6 md:leading-normal">Simplified progress tracking, for you!</h1>
             <p className="text-lg md:text-xl mb-4">Track. Achieve. Repeat</p>
