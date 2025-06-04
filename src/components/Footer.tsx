68977d389a32d5390568b2f6dad1b3297e16785c
Michael Gyimadu <michaelgyimadu@gmail.com>
2025-06-04 13:15:50 -0400

    updated homepage and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,20 +5,11 @@ const Footer: React.FC = () => {
     <footer className="container mx-auto max-w-7xl px-4 py-16">
         <div className="grid grid-cols-1 mx-auto w-full md:grid-cols-3 gap-8 border-t-2 pt-10">
             <div className="space-y-4">
-                <h3 className="text-sm font-semibold text-gray-800">HABITS</h3>
-                <p className="text-sm text-gray-600">© 2024 Habits-io</p>
+                <h3 className="text-sm font-semibold text-gray-800">Zuno</h3>
+                <p className="text-sm text-gray-600">© 2024 Zuno-io</p>
             </div>
             
-            <div className="grid grid-cols-2 gap-8">
-                <div>
-                    <h4 className="text-sm font-semibold text-gray-700 mb-4">Resources</h4>
-                    <ul className="space-y-2 text-sm text-gray-600">
-                    <li className="hover:text-gray-800 transition-colors cursor-pointer">Help Center</li>
-                    <li className="hover:text-gray-800 transition-colors cursor-pointer">Blog</li>
-                    <li className="hover:text-gray-800 transition-colors cursor-pointer">Community</li>
-                    </ul>
-                </div>
-            
+            <div className="grid grid-cols-2 gap-8">  
                 <div>
                     <h4 className="text-sm font-semibold text-gray-700 mb-4">Company</h4>
                     <ul className="space-y-2 text-sm text-gray-600">
