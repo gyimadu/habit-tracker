11979e2dbb1620ec1fc7983026d0309d607a3e3c
Michael Gyimadu <michaelgyimadu@gmail.com>
2025-06-04 14:57:17 -0400

    minor changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -73,7 +73,7 @@ export default function NavBar() {
       {/* Mobile Menu */}
       <div 
         className={`md:hidden fixed top-[4rem] left-0 right-0 bg-white shadow-lg transition-all duration-300 ease-in-out transform ${
-          isMenuOpen ? 'translate-y-0 opacity-90' : '-translate-y-4 opacity-0 pointer-events-none'
+          isMenuOpen ? 'translate-y-0 opacity-95' : '-translate-y-4 opacity-0 pointer-events-none'
         }`}
       >
         <div className="container mx-auto px-4 py-4">
