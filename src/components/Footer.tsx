4d614f262cc6f5fbe8b0f60f0671c6327bbb4ff8
Michael Gyimadu <michaelgyimadu@gmail.com>
2025-06-04 13:13:40 -0400

    updated homepage and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,16 +29,6 @@ const Footer: React.FC = () => {
                     </ul>
                 </div>
             </div>
-            
-            <div>
-                <h4 className="text-sm font-semibold text-gray-700 mb-4">Follow Us</h4>
-                <ul className="space-y-2 text-sm text-gray-600">
-                    <li className="hover:text-gray-800 transition-colors cursor-pointer">Twitter</li>
-                    <li className="hover:text-gray-800 transition-colors cursor-pointer">Instagram</li>
-                    <li className="hover:text-gray-800 transition-colors cursor-pointer">LinkedIn</li>
-                    <li className="hover:text-gray-800 transition-colors cursor-pointer">Facebook</li>
-                </ul>
-            </div>
         </div>
     </footer>
   );
