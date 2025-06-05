4193db06695d034f850bbe48cab192f0510fd583
Michael Gyimadu <michaelgyimadu@gmail.com>
2025-06-05 14:46:02 -0400

    updated footer on mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="border-t border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="flex items-center space-x-4 mb-4 md:mb-0">
+          <div className="flex flex-col md:flex-row items-center space-x-4 justify-between mb-4 md:mb-0">
             <Link href="/" className="text-lg md:text-2xl font-bold text-blue-600">
               Grit
             </Link>
